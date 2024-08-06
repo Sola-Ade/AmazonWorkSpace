3b6c63edf06827a4f96db949dfa86ce41f7cd0ff
Sola-Ade <solaadefarasin@hotmail.com>
2024-08-06 13:11:06 +0100

    add

---
+++ Scrimba/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components.js/navbar";
+import Main from "./components.js/main";
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
+
---
+++ Scrimba/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// const navbar = (
+//     <nav>
+//         <h1>Bob's Bistro</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.render(navbar, document.getElementById("root"))
+
+// // firstly import the below:
+// import React from "react"
+// import ReactDOM from "reatc-dom"
+
+// //create a variable called page
+// const page = (
+//     <div>
+//         <img src="./react-logo.png" width="40px" alt="React logo"/>
+//         <h1>Fun Facts</h1>
+//             <ul>
+//                 <li>About Elephants</li>
+//                 <li>About Humans</li>
+//                 <li>About Giraffes</li>
+//             </ul>
+//     </div>
+// ) 
+
+// //To make sure it shows on the page.
+// ReactDOM.render(page, document.getElementById("root"))
